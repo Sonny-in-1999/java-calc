f0494f328ccebe91f8d60bc9b10d8f4f16148c26
Sonny-in-1999 <minsoo1022@gmail.com>
2025-02-27 15:42:50 +0900

    feat: LV.3 계산기를 위한 클래스 생성 및 README.md 수정

---
+++ src/level3/Calculator.java
@@ -0,0 +1,75 @@
+package level3;
+
+import level2.Result;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    private int number1;
+    private int number2;
+    private char operator;
+    private List<Result> resultList = new ArrayList<>();
+
+    // this()
+    public Calculator() {
+    }
+
+    // this: 객체가 자기 자신을 가리키는 레퍼런스 변수
+    // 필드값과 동일한 이름의 매개변수를 받을 경우, 필드값에 this를 붙여 객체의 필드값임을 구별해주어야 합니다.
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
+
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
+
+    public void setOperator(char operator) {
+        this.operator = operator;
+    }
+
+    public List<Result> getResultMap() {
+        return resultList;
+    }
+
+    public void calculate() throws IllegalArgumentException {
+        int result;
+        LocalDateTime now = LocalDateTime.now();
+        switch (operator) {
+            case '+': {
+                result = number1 + number2;
+                Result resultListItem = new Result(number1 + " " + operator + " " + number2, result, now);
+                resultList.add(resultListItem);
+                System.out.println("결과: " + result);
+                break;
+            }
+            case '-': {
+                result = number1 - number2;
+                Result resultListItem = new Result(number1 + " " + operator + " " + number2, result, now);
+                resultList.add(resultListItem);
+                System.out.println("결과: " + result);
+                break;
+            }
+            case '*': {
+                result = number1 * number2;
+                Result resultListItem = new Result(number1 + " " + operator + " " + number2, result, now);
+                resultList.add(resultListItem);
+                System.out.println("결과: " + result);
+                break;
+            }
+            case '/': {
+                result = number1 / number2;
+                Result resultListItem = new Result(number1 + " " + operator + " " + number2, result, now);
+                resultList.add(resultListItem);
+                System.out.println("결과: " + result);
+                break;
+            }
+            default: {
+                throw new IllegalArgumentException("올바른 기호를 입력해주세요.");
+            }
+        }
+    }
+}
---
+++ src/level3/Result.java
@@ -0,0 +1,7 @@
+package level3;
+
+import java.time.LocalDateTime;
+
+public record Result(String operate, int result, LocalDateTime createdAt) {
+
+}
